1992173ec1869f4a41ad156300997834666ab150
androsrivas <androsrivas@outlook.com>
2026-05-07 22:26:02 +0200

    feat: add navigation menu components for admin, gestoras, particulares, and tecnicos

---
+++ resources/views/components/navigation/menus/admin.blade.php
@@ -0,0 +1,7 @@
+<x-navigation.sidebar-link route="calendario" icon="📆">Calendario</x-navigation.sidebar-link>
+<x-navigation.sidebar-link route="incidencias.index" icon="📋">Incidencias</x-navigation.sidebar-link>
+{{-- <x-navigation.sidebar-link route="clientes.index" icon="👥">Clientes</x-navigation.sidebar-link> --}}
+{{-- <x-navigation.sidebar-link route="tecnicos.index" icon="👨‍🔧">Técnicos</x-navigation.sidebar-link> --}}
+{{-- <x-navigation.sidebar-link route="especialidades.index" icon="🔧">Especialidades</x-navigation.sidebar-link> --}}
+{{-- <x-navigation.sidebar-link route="gestoras.index" icon="🏢">Gestoras</x-navigation.sidebar-link> --}}
+{{-- <x-navigation.sidebar-link route="comisiones.index" icon="💰">Comisiones</x-navigation.sidebar-link> --}}
---
+++ resources/views/components/navigation/menus/particulares.blade.php
@@ -0,0 +1,3 @@
+<x-navigation.sidebar-link route="cliente.perfil" icon="👤">Mi Perfil</x-navigation.sidebar-link>
+<x-navigation.sidebar-link route="cliente.nueva-incidencia" icon="➕">Nueva incidencia</x-navigation.sidebar-link>
+<x-navigation.sidebar-link route="cliente.incidencias" icon="📋">Mis Servicios</x-navigation.sidebar-link>
